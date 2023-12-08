40da04b726c9d826442d305e44211866a97196f8
shinewwaitun <shinewaitun28@gmail.com>
2023-12-08 15:58:18 +0630

    fix image size for article card

--- src/App.js
+++ src/App.js
@@ -8,19 +8,19 @@ function App() {
   const title = "Here are something you should know regarding how we work";
   const articles = [
     {
-      img: img,
+      img: "https://blog.lingoda.com/wp-content/uploads/2021/07/mexican-traditions-scaled-e1627035111698.jpg",
       title: title
     },
     {
-      img: img,
+      img: "https://d2jx2rerrg6sh3.cloudfront.net/images/Article_Images/ImageForArticle_22572_16533781526443229.jpg",
       title: title
     },
     {
-      img: img,
+      img: "https://images.squarespace-cdn.com/content/v1/5aa96c579772aea9adaa2ef7/62728ff0-bf16-4bcf-b5b4-c9b1bb0d152d/AIinHealthcare_723+%281%29.png",
       title: title
     },
     {
-      img: img,
+      img: "https://i.insider.com/5fd8ea51dc030c001aab6bae?width=700",
       title: title
     }
   ];
