b93d22d86ea6e36d4841cdc34cc35db8bc22cc17
Lin Thit <linthit2745@gmail.com>
2024-01-22 15:06:22 +0630

    fix profile page

--- src/components/ProfileInfo.jsx
+++ src/components/ProfileInfo.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 function ProfileInfo({ user }) {
   return (
-    <div className="py-4 my-4 shadow d-flex flex-row justify-content-evenly align-items-center">
-      <div className="d-flex flex-column justify-content-start align-items-start">
+    <div className="py-4 my-4 shadow d-flex flex-row justify-content-center align-items-center">
+      <div className="d-flex flex-column justify-content-start align-items-start w-50 ps-5">
         <div className="py-4">
           <img
             className="rounded-circle"
@@ -13,13 +13,20 @@ function ProfileInfo({ user }) {
             alt="profile_image"
           />
         </div>
-        <div>USERNAME : {user.name}</div>
-      </div>
-      <div className="d-flex flex-column justify-content-center align-items-start align-self-end gap-4">
-        <div>
-          Status : <span className="text-success">ACTIVE</span>
+        <div className="w-100 d-flex justify-content-center">
+          <span className="w-50">USERNAME</span>
+          <span className="w-50">: Jessica Everest</span>
         </div>
-        <div>EMAIL : jessica@gmail.com</div>
+      </div>
+      <div className="d-flex flex-column justify-content-center align-items-start align-self-end w-50 gap-4">
+        <div className="w-100 d-flex justify-content-center">
+          <span className="w-50">STATUS</span>
+          <span className="text-success w-50">: ACTIVE</span>
+        </div>
+        <div className="w-100 d-flex justify-content-center">
+          <span className="w-50">EMAIL</span>
+          <span className="w-50">: jessica@gmail.com</span>
+        </div>
       </div>
     </div>
   );
